da305a75a5115fa5733fe50a7e4eb3a88281cb8f
Gersitz <125976146+Gersitz@users.noreply.github.com>
2023-09-12 11:14:46 +0200

    Init

---
+++ Taschenrechner/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Willkommen beim Taschenrechner 3000");
+bool checkParse1 = false;
+bool checkParse2 = false;
+int num1 = 0;
+int num2 = 0;
+
+while (!checkParse1 || !checkParse2)
+{
+    Console.WriteLine("Bitte gebe Zahl 1 an:");
+    string input1 = Console.ReadLine();
+
+    Console.WriteLine("Bitte gebe Zahl 2 an:");
+    string input2 = Console.ReadLine();
+
+    // Konvertierung von Text zu Zahlen
+    checkParse1 = int.TryParse(input1, out num1);
+    checkParse2 = int.TryParse(input2, out num2);
+}
+
+var sum = num1 + num2;
+Console.WriteLine($"{num1} + {num2} = {sum}");
