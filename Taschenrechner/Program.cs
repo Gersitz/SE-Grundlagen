e8f8925beb54c5cdb21823faac5b014e56dd6fbb
Gersitz <125976146+Gersitz@users.noreply.github.com>
2023-09-15 11:19:18 +0200

    added project for explaining variable declarations and ref vs value types improved Taschenrechner with support for multiple calculations, floating-point-numbers and fixed divide-by-0-error

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -1,14 +1,15 @@
-﻿using System.Numerics;
+﻿using System.Net.Http.Headers;
+using System.Numerics;
 
-int GetNumber()
+decimal GetNumber()
 {
-    int num = 0;
+    decimal num = 0;
     bool checkParse = false;
     do
     {
         Console.WriteLine("Bitte gebe eine Zahl an:");
         string input = Console.ReadLine();
-        checkParse = int.TryParse(input, out num);
+        checkParse = decimal.TryParse(input, out num);
 
         if (checkParse == false)
         {
@@ -39,7 +40,7 @@ string GetOperator()
     return mathoperator;
 }
 
-int Calculate(int num1, int num2, string mathoperator)
+decimal Calculate(decimal num1, decimal num2, string mathoperator)
 {
     switch (mathoperator)
     {
@@ -50,11 +51,6 @@ int Calculate(int num1, int num2, string mathoperator)
         case "*":
             return num1 * num2;
         case "/":
-            if (num2 == 0)
-            {
-                Console.WriteLine("Durch 0 kann nicht geteilt werden");
-                break;
-            }
             return num1 / num2;
         default:
             Console.WriteLine("Fehlerhafte Eingabe, mögliche Werte: + - * /");
@@ -63,9 +59,44 @@ int Calculate(int num1, int num2, string mathoperator)
 }
 
 Console.WriteLine("Willkommen beim Taschenrechner 3000");
-var num1 = GetNumber();
-var num2 = GetNumber();
-var operation = GetOperator();
+bool restartCalc = false;
 
-var result = Calculate(num1, num2, operation);
-Console.WriteLine($"{num1} {operation} {num2} = {result}");
+do
+{
+    restartCalc = false;
+    var num1 = GetNumber();
+    var num2 = GetNumber();
+    var operation = GetOperator();
+
+    while (num2 == 0 && operation == "/")
+    {
+        Console.WriteLine("Teilen durch 0 nicht möglich! Bitte wähle die zweite Zahl und den Operator erneut.");
+        num2 = GetNumber();
+        operation = GetOperator();
+    }
+        var result = Calculate(num1, num2, operation);
+        Console.WriteLine($"{num1} {operation} {num2} = {result}");
+
+    Console.WriteLine("Noch eine Berechnung durchführen? (J/N)");
+    var retry = Console.ReadLine();
+    do
+    {
+        if (retry == "J")
+        {
+            Console.WriteLine("Taschenrechner wird neu gestartet...");
+            restartCalc = true;
+            break;
+        }
+        else if (retry == "N")
+        {
+            Console.WriteLine("Taschenrechner wird beendet...");
+            restartCalc = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Bitte J oder N eingeben.");
+            retry = Console.ReadLine();
+        }
+    } while (retry != "J" || retry != "N");
+} while (restartCalc == true);
---
+++ VariablenUndDatentypen/Program.cs
@@ -0,0 +1,81 @@
+﻿#region Variablen
+// Variablen Deklaration
+string name;
+int age;
+object random;
+float height;
+// Variablen sind erst nach einer Deklaration verwendbar
+
+// Console.WriteLine(age); => Funktioniert erst nach der Initialisierung
+
+// Variablen Initialisierung
+name = "Default";
+age = 0;
+random = new object();
+height = 0.0f;
+
+// Deklaration und Initialisierung
+int abc = 42;
+string hello = "Hello";
+var test = Console.ReadLine();
+
+int i = 0;
+while(i<5)
+{
+    //var xyz = 10;
+    Console.WriteLine(hello);
+    i++;
+} // Wenn ein Codeblock geschlossen wird, sind alle Variablen,
+// die innerhalb des Blocks deklariert wurden, nicht mehr gültig.
+// -> Können danach nicht mehr verwendet werden.
+
+// xyz -> Geht nicht, da xyz in anderem Scope ist.
+
+var xyz = 20; // -> Variable kann neu deklariert werden
+
+#endregion
+
+#region Primitive-Datentypen
+// Primitive Datentypen -> Werte-Typen, Value-Types
+bool boolean = false || true;
+int ganzzahl = 200;
+ganzzahl = 500;
+//Int16 test = 0; -> Würde man nie in der Praxis verwenden,
+//der "normale" int ist dynamisch in C#
+float width = 1.0f;
+double depth = 1.0;
+decimal geld = 90000;
+var testfloat = 1.0;
+char sign = '+';
+char letter = 'B';
+#endregion
+
+#region Komplexe-Datentypen
+// Referenz-Typen -> Referenz-Typen
+object myObject = new object();
+string greeting = "Hello World!";
+// dynamic myDynamic = new dynamic();
+#endregion
+
+#region WerteVsReferenz-Typen
+int num1 = 10;
+
+int[] array1 = { 1, 2, 3 };
+
+Console.WriteLine("Vor Modify");
+Console.WriteLine("num1: " + num1);
+Console.WriteLine("array1: " + string.Join(',', array1));
+
+Modify(num1, array1);
+
+Console.WriteLine("Nach Modify");
+Console.WriteLine("num1: " + num1);
+Console.WriteLine("array1: " + string.Join(',', array1));
+
+void Modify(int num, int[] array)
+{
+    num = 100;
+    array[0] = 100;
+}
+
+#endregion
