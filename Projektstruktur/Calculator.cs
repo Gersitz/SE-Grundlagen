cc6e86840fc90bafd84eef8820cea5b46a332996
Gersitz <125976146+Gersitz@users.noreply.github.com>
2023-09-18 09:43:44 +0200

    added Projektstruktur

---
+++ Projektstruktur/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    internal static class Calculator
+    {
+        internal static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        internal static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ Projektstruktur/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyCalculator;
+namespace Projektstruktur;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var num1 = 10;
+        var num2 = 20;
+
+        var addResult = Calculator.Add(num1, num2);
+        Console.WriteLine(addResult);
+    }
+}
