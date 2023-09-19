6160271d8393e41cf92e17a00d603753719a32e7
Gersitz <125976146+Gersitz@users.noreply.github.com>
2023-09-19 15:58:15 +0200

    added Algorithms and TestAlgorithms

--- TestAlgorithms/UnitTest1.cs
+++ TestAlgorithms/UnitTest1.cs
@@ -11,5 +11,32 @@ namespace TestAlgorithms
 
             Assert.Equal(2, CardAlgorithms.FindCardLocation(cards, query));
         }
+
+        [Fact]
+        public void TestLocateCard_QueryNotInCards()
+        {
+            int[] cards = { -1, 1 };
+            int query = 0;
+
+            Assert.Equal(-1, CardAlgorithms.FindCardLocation(cards, query));
+        }
+
+        [Fact]
+        public void TestLocateCard_DuplicateQueryInCards()
+        {
+            int[] cards = { 0, 1, 2, 3, 21, 21, 42, 69, 420 };
+            int query = 21;
+
+            Assert.Equal(4, CardAlgorithms.FindCardLocation(cards, query));
+        }
+
+        [Fact]
+        public void TestLocateCard_CardsIsEmpty()
+        {
+            int[] cards = { };
+            int query = 69;
+
+            Assert.Equal(-1, CardAlgorithms.FindCardLocation(cards, query));
+        }
     }
-}
+} aaaaaa
