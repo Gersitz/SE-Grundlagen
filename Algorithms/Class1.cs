995bc1303082ff2445888f22550a7942e8f9eeb7
Gersitz <125976146+Gersitz@users.noreply.github.com>
2023-09-20 10:06:01 +0200

    Testcommit

--- Algorithms/Class1.cs
+++ Algorithms/Class1.cs
@@ -4,7 +4,25 @@
     {
         public static int FindCardLocation(int[] cards, int query)
         {
-            return -1;
+            int index = 0;
+            while (index < cards.Length)
+            {
+                if (cards[index] == query)
+                {
+                    return index;
+                }
+                else if (query == 0)
+                { 
+                    break;
+                }
+                index++;
+            }
+            return index -1;
+           // initialisiere Variable index mit Wert 0
+           // Schleife bis index gleich Länge der cards Liste
+           //     Wenn Zahl von Cards an index gleich query:
+           //         Wahr: Gebe index zurück
+           //     Inkrement index um 1
         }
     }
 }
