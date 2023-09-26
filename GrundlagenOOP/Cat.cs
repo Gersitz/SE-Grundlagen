bd9e49293780fc0f5d6b6c4562460c09e6f08bb3
Gersitz <125976146+Gersitz@users.noreply.github.com>
2023-09-26 11:58:34 +0200

    Grundlagen zu Klassen und Objekten, Static, sowohl als auch Instanz

---
+++ GrundlagenOOP/Cat.cs
@@ -0,0 +1,31 @@
+﻿namespace GrundlagenOOP;
+public class Cat
+{
+  public string Name;
+  public int Age;
+  public string Color;
+
+ 
+
+  // Konstruktor -> Erstellt eine neue Instanz der Klasse Cat
+  // -> Nicht für static-classes / statische Klassen
+  public Cat(string name, int age, string color)
+  {
+    this.Name = name;
+    this.Age = age;
+    this.Color = color;
+  }
+  public void Miau()
+  {
+    Console.WriteLine("Miau!");
+  }
+  public static void Schnurren()
+  {
+    Console.WriteLine("purrrrrrr...");
+  }
+  public override string ToString()
+  {
+    // $"Test{x}" -> String-Interpolation
+    return $"Name: {this.Name}\nAge: {this.Age}\nColor: {this.Color}";
+  }
+}
---
+++ GrundlagenOOP/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace GrundlagenOOP;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!"); // Statische-Klasse
+        Random rng = new Random(); // Instanziierbare-Klasse
+        // Cat myCat = new Cat();
+        int x;
+        Cat y; // Cat ist unser eigens erstellter Datentyp
+        // myCat.Meow();
+        // myCat.Name = "Minka";
+        // myCat.Age = 10;
+        // myCat.Color = "Silver-Tabby";
+        // Cat myCoolCat = new Cat();
+        // myCoolCat.Color = "Brown";
+        // myCoolCat.Age = 12;
+        // myCoolCat.Name = "Tom";
+        // Console.ReadLine();
+        // Console.WriteLine(myCat);
+        // Console.WriteLine(myCoolCat);
+        // myCoolCat.Name = "Stefan";
+        // Console.WriteLine(myCoolCat);
+
+        Cat jerry = new Cat("jerry", 8, "black");
+        Console.WriteLine(jerry);
+
+        jerry.Age += 1;
+
+        jerry = new Cat("minka", 12, "silver");
+        Console.WriteLine(jerry);
+        jerry.Miau();
+        Cat.Schnurren();
+        Console.WriteLine(jerry);
+        Console.ReadLine();
+    }
+}
