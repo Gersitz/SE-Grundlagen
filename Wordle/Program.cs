90e0fe7f989f38ed74b27ca09a2e82d878c1505d
Gersitz <125976146+Gersitz@users.noreply.github.com>
2023-09-27 14:49:18 +0200

    Start des Wordles

---
+++ Wordle/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Programmstart:
+ * 
+ * Definiere solutionWord als das zu erratende Wort
+ * 
+ * Schleife:
+ *     Definiere guess und speicher das vom User eingegebene Wort
+ *     Schleife für jeden Buchstaben in guess:
+ *         Schleife für jeden Buchstaben in solutionWord:
+ *             Prüfe Buchstabe aus guess gleich Buchstabe in solutionWord:
+ *                 Wahr: 
+ */
+
+var solutionWord = "HOTEL";
+
+string guess;
+
+while (true)
+{
+    Console.WriteLine("Errate das 5-stellige Wort");
+    guess = Console.ReadLine();
+    if (guess.Length != 5) continue;
+
+    // foreach (char c in guess)
+    // {
+    //     if (solutionWord.Contains(c))
+    //         Console.BackgroundColor = ConsoleColor.Yellow;
+    //     if (/*...*/)
+    //         Console.BackgroundColor = ConsoleColor.Green;
+    //     Console.WriteLine($"{c}");
+    // }
+
+    for (int i = 0; i < guess.Length; i++)
+    {
+        if (solutionWord.Contains(guess[i]))
+            Console.BackgroundColor = ConsoleColor.Yellow;
+        if (solutionWord[i] == guess[i])
+            Console.BackgroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{guess[i]}");
+    }
+}
