80c48f257e7855a3d963a667f54dab8b1e818d5d
Gersitz <125976146+Gersitz@users.noreply.github.com>
2023-09-28 11:53:28 +0200

    added more functionality to Wordle

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -10,15 +10,31 @@
  *             Prüfe Buchstabe aus guess gleich Buchstabe in solutionWord:
  *                 Wahr: 
  */
+using System.Runtime.CompilerServices;
 
-var solutionWord = "HOTEL";
+var solutionWordList = new List<string>();
+using (var reader = new StreamReader(@"C:\\Users\\KevinGersitz\\Documents\\Anwendungsentwicklung\\Source\\SE-Grundlagen\\Wordle\\words\\valid_solutions.csv"))
+{
+    while (reader.EndOfStream == false)
+    {
+        var line = reader.ReadLine();
+        solutionWordList.Add(line);
+    }
+}
+
+Random rng = new Random();
+var nextSolutionIndex = rng.Next(0, solutionWordList.Count);
+
+var solutionWord = solutionWordList[nextSolutionIndex];
+solutionWord = solutionWord.ToUpper();
 
 string guess;
 
 while (true)
 {
-    Console.WriteLine("Errate das 5-stellige Wort");
+    Console.WriteLine("Guess the word");
     guess = Console.ReadLine();
+    guess = guess.ToUpper();
     if (guess.Length != 5) continue;
 
     // foreach (char c in guess)
@@ -30,12 +46,21 @@ while (true)
     //     Console.WriteLine($"{c}");
     // }
 
+    /* ToDo:
+     * Groß- und Kleinschreibung
+     */
+
+    Console.ForegroundColor = ConsoleColor.Black;
     for (int i = 0; i < guess.Length; i++)
     {
+        Console.BackgroundColor = ConsoleColor.White;
         if (solutionWord.Contains(guess[i]))
             Console.BackgroundColor = ConsoleColor.Yellow;
         if (solutionWord[i] == guess[i])
             Console.BackgroundColor = ConsoleColor.Green;
-        Console.WriteLine($"{guess[i]}");
+        Console.Write($"{guess[i]}");
     }
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.WriteLine();
 }
