133381389226615881be4fce126475c7a851f3d3
Gersitz <125976146+Gersitz@users.noreply.github.com>
2023-10-05 16:17:16 +0200

    Added more functionality to RPG

--- RPG/Character.cs
+++ RPG/Character.cs
@@ -1,33 +1,71 @@
-﻿namespace RPG;
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RPG;
 public class Character
 {
     public string Name { get; protected set; }
+    public string Gender { get; set; }
     public int Level { get; set; }
     public int Health { get; set; }
-    public int Mana { get; set; }
     public int AttackPower { get; set; }
     public int MagicAttackPower { get; set; }
     public int Defense { get; set; }
     public int MagicDefense { get; set; }
     public int Speed { get; set; }
+    public int Dodge { get; set; }
 
-    public Character(string name, int level, int health, int mana, int attack, int magicattack, int defense, int magicdefense, int speed)
+    public Character(string name, string gender, int level, int health, int attack, int magicattack, int defense, int magicdefense, int speed, int dodge)
     {
         Name = name;
+        Gender = gender;
         Level = level;
         Health = health;
-        Mana = mana;
         AttackPower = attack;
         MagicAttackPower = magicattack;
         Defense = defense;
         MagicDefense = magicdefense;
         Speed = speed;
+        Dodge = dodge;
     }
-    public virtual void Attack(Character target)
+    public virtual void Attack(Character target, string skillType, string damageCalc, int dmg, double multiplier)
     {
-        int damage = CalculateDamage(target);
-        target.Defend(damage);
-        Console.WriteLine($"{Name} attacked {target.Name}! Damage dealt: {damage}");
+        if (skillType == "basic")
+        {
+            int damage = CalculateDamage(target);
+            target.Defend(damage);
+            Console.WriteLine($"Damage dealt: {damage}");
+        }
+        else if (skillType == "physical")
+        {
+            if (damageCalc == "additive")
+            {
+                int damage = CalculateSkillDamage(target, dmg);
+                target.Defend(damage);
+                Console.WriteLine($"Damage dealt: {damage}");
+            }
+            else if (damageCalc == "multiplicative")
+            {
+                int damage = CalculateSkillDamagePercent(target, multiplier);
+                target.Defend(damage);
+                Console.WriteLine($"Damage dealt: {damage}");
+            }
+        }
+        else if (skillType == "magic")
+        {
+            if (damageCalc == "additive")
+            {
+                int damage = CalculateMagicDamage(target, dmg);
+                target.Defend(damage);
+                Console.WriteLine($"Damage dealt: {damage}");
+            }
+            else if (damageCalc == "multiplicative")
+            {
+                int damage = CalculateMagicDamagePercent(target, multiplier);
+                target.Defend(damage);
+                Console.WriteLine($"Damage dealt: {damage}");
+            }
+        }
     }
     public virtual void Defend(int damage)
     {
@@ -41,10 +79,71 @@ public class Character
     {
         int damage = AttackPower - target.Defense;
         return damage > 0 ? damage : 0;
+
     }
-    protected int CalculateMagicDamage(Character target)
+    protected int CalculateSkillDamage(Character target, int dmg)
     {
-        int damage = MagicAttackPower - target.MagicDefense;
+        int damage = AttackPower + dmg - target.Defense;
         return damage > 0 ? damage : 0;
+    }
+    protected int CalculateSkillDamagePercent(Character target, double multiplier)
+    {
+        int damage = (int)(AttackPower * multiplier);
+        return damage > 0 ? damage : 0;
+    }
+    protected int CalculateMagicDamage(Character target, int dmg)
+    {
+        int damage = MagicAttackPower + dmg - target.MagicDefense;
+        return damage > 0 ? damage : 0;
+    }
+    protected int CalculateMagicDamagePercent(Character target, double multiplier)
+    {
+        int damage = (int)(MagicAttackPower * multiplier);
+        return damage > 0 ? damage : 0;
+
+    }
+    protected string MissingRessources(string ressource)
+    {
+        string prompt = $"Not enough {ressource}!";
+        return prompt;
+    }
+    public void IncreaseLevel()
+    {
+        if (Level < 10)
+        {
+            Level += 1;
+            Health += 10;
+            AttackPower += 2;
+            MagicAttackPower += 2;
+            Defense += 1;
+            MagicDefense += 1;
+        }
+        else if (Level < 30)
+        {
+            Level += 1;
+            Health += 15;
+            AttackPower += 3;
+            MagicAttackPower += 3;
+            Defense += 2;
+            MagicDefense += 2;
+        }
+        else if (Level < 50)
+        {
+            Level += 1;
+            Health += 20;
+            AttackPower += 4;
+            MagicAttackPower += 4;
+            Defense += 2;
+            MagicDefense += 2;
+        }
+        else
+        {
+            Level += 1;
+            Health += 25;
+            AttackPower += 5;
+            MagicAttackPower += 5;
+            Defense += 3;
+            MagicDefense += 3;
+        }
     }
 }
--- RPG/Classes.cs
+++ RPG/Classes.cs
@@ -1,12 +1,18 @@
 ﻿namespace RPG;
-public class Classes
+public static class Classes
 {
     public class Warrior : Character
     {
         public int Rage { get; private set; }
-        public Warrior(string name) : base(name, level: 1, health: 200, mana: 100, attack: 35, magicattack: 10, defense: 10, magicdefense: 5, speed: 100)
+        public Warrior(string name, string gender) : base(name, gender, level: 1, health: 200, attack: 35, magicattack: 10, defense: 10, magicdefense: 5, speed: 100, dodge: 2)
         {
             Rage = 0;
+        }
+        public void BasicAttack(Character target)
+        {
+            Console.WriteLine($"{Name} casts attacks {target.Name}!");
+            Attack(target, "basic", "", 0, 1.0);
+            Rage += 5;
         }
         public void Berserk()
         {
@@ -17,46 +23,39 @@ public class Classes
             Rage += 10;
             Console.WriteLine($"{Name} used Berserk!\nAttack: + {attackChange}\nDefense: - {defenseChange}\nRage: + 10");
         }
-        public void Ragepunch(Character target)
+        public void Ragefist(Character target)
         {
-            AttackPower += AttackPower * 110 / 100;
-            int damage = CalculateDamage(target);
+            int damage = CalculateSkillDamagePercent(target, 10);
             target.Defend(damage);
             target.Defense -= 5;
             Rage -= 20;
-            Console.WriteLine($"{Name} used Ragepunch!\nDamage dealt: {damage}\n{target.Name}s Defense: -5");
-        }
-        public override void Attack(Character target)
-        {
-            int damage = CalculateDamage(target);
-            target.Defend(damage);
-            Rage += 5;
-            Console.WriteLine($"{Name} attacked {target.Name}! Damage dealt: {damage}\nRage: + 5");
+            Console.WriteLine($"{Name} used Ragefist!\nDamage dealt: {damage}\n{target.Name}s Defense: -5");
         }
     }
 
 
     public class Mage : Character
     {
-        public Mage(string name) : base(name, level: 1, health: 100, mana: 150, attack: 10, magicattack: 35, defense: 5, magicdefense: 10, speed: 100)
+        public int Mana { get; set; }
+        public Mage(string name, string gender) : base(name, gender, level: 1, health: 100, attack: 10, magicattack: 35, defense: 5, magicdefense: 10, speed: 100, dodge: 2)
         {
-
+            Mana = 0;
         }
         private void CastMagicShield()
         {
             int defenseChange = (int)(Defense * 0.1);
-            int mdefenseChange = (int)(MagicDefense * 0.25);
+            int magicDefenseChange = (int)(MagicDefense * 0.25);
             Defense += defenseChange;
-            MagicDefense += mdefenseChange;
+            MagicDefense += magicDefenseChange;
             Mana -= 30;
-            Console.WriteLine($"{Name} casts a magical shield!\nDefense: + {defenseChange}\nMagic Defense: + {mdefenseChange}");
+            Console.WriteLine($"{Name} casts a magical shield!\nDefense: + {defenseChange}\nMagic Defense: + {magicDefenseChange}");
         }
         public void CastFireball(Character target)
         {
-            int damage = CalculateMagicDamage(target);
-            target.Defend(damage);
+            Console.WriteLine($"{Name} casts Fireball on {target.Name}!");
+            Attack(target, "magic", "multiplicative", 0, 1.5);
             Mana -= 15;
-            Console.WriteLine($"{Name} casts Fireball on {target.Name}!\nDamage dealt: {damage}");
+
             if (RandomChance(0.10))
             {
                 ApplyBurn(target);
@@ -75,39 +74,37 @@ public class Classes
 
     public class Rogue : Character
     {
-        private bool activeStealth = false;
-        public Rogue (string name) : base(name, level: 1, health: 150, mana: 100, attack: 20, magicattack: 10, defense: 10, magicdefense: 5, speed: 120)
+        public bool activeStealth = false;
+        public int Shadowforce { get; set; }
+        public Rogue(string name, string gender) : base(name, gender, level: 1, health: 150, attack: 20, magicattack: 10, defense: 10, magicdefense: 5, speed: 120, dodge: 2)
         {
-
+            Shadowforce = 0;
         }
-        public override void Attack(Character target)
+        public override void Attack(Character target, string skillType, string damageCalc, int dmg, int multiplier)
         {
             if (activeStealth)
             {
-                int attackChange = (int)(AttackPower * 0.3);
-                AttackPower += attackChange;
+                AttackPower *= (int)1.3;
                 int damage = CalculateDamage(target);
                 activeStealth = false;
                 Console.WriteLine($"{Name} attacked {target.Name} out of the shadows! Stealth ended.\nDamage dealt: {damage}");
             }
             else
             {
-                base.Attack(target);
+                base.Attack(target, skillType, damageCalc, dmg, multiplier);
             }
         }
         public override void Defend(int damage)
         {
             if (activeStealth)
             {
-                double randomValue = new Random().NextDouble();
-
-                if (randomValue < 0.75)
+                if (RandomChance(0.75))
                 {
                     Console.WriteLine($"{Name} dodged the attack!");
                 }
                 else
                 {
-                    Console.WriteLine($"{Name} was hit and the Stealth stance ended.");
+                    Console.WriteLine($"{Name} was hit and the stealth stance ended.");
                     base.Defend(damage);
                     activeStealth = false;
                 }
@@ -115,8 +112,30 @@ public class Classes
         }
         public void CastStealth()
         {
-            Console.WriteLine($"{Name} used Stealth and became hard to see!");
-            activeStealth = true;
+            if (Shadowforce >= 45)
+            {
+                Console.WriteLine($"{Name} used Stealth and became hard to see!");
+                activeStealth = true;
+                Shadowforce -= 45;
+            }
+            else
+            {
+                MissingRessources("Shadowforce");
+            }
+        }
+        public void CastBackstab(Character target)
+        {
+            if (activeStealth)
+            {
+                int CurrentAttackPower = AttackPower;
+                AttackPower = (int)(AttackPower * 1.5);
+                int damage = CalculateDamage(target);
+                AttackPower = CurrentAttackPower;
+            }
+            else
+            {
+                base.CalculateDamage(target);
+            }
         }
     }
     public static bool RandomChance(double chance)
